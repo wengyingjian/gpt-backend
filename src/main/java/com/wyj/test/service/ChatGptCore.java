2c00c2c671308f89d38e4c56346aa8624b2dfedd
wyj <wengyingjian@gmail.com>
2023-03-28 22:01:11 +0800

    add init

--- src/main/java/com/wyj/test/service/ChatGptCore.java
+++ src/main/java/com/wyj/test/service/ChatGptCore.java
@@ -17,33 +17,6 @@ import java.util.List;
 @Service
 public class ChatGptCore {
 
-
-    public void init() {
-        Proxy proxy = Proxys.http("127.0.0.1", 7890);
-
-        ChatGPT chatGPT = ChatGPT.builder()
-                .apiKey("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")
-                .timeout(900)
-                .proxy(proxy)
-                .apiHost("https://api.openai.com/") //代理地址
-                .build()
-                .init();
-
-        CreditGrantsResponse response = chatGPT.creditGrants();
-        System.out.println("余额：{}" + response.getTotalAvailable());
-    }
-
-    //max_tokens: 1000,
-    //model: 'gpt-3.5-turbo',
-    //temperature: 0.8,
-    //top_p: 1,
-    //presence_penalty: 1,
-
-    //{ role: 'user', content: '我上上上句说了什么', name: undefined },
-
-    //parent_id: "chatcmpl-6yrZmkseGo0HPdeFjHVPbCrOkoIpk"
-
-
     @Value("${chatgpt.apikey}")
     private String chatGptApiKey;
 
